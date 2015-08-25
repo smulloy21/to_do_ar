4be36f17ffd7bc38ae347f06eb2ecdd3842be3b0
Shannon Mulloy <smulloy21@gmail.com>
2015-08-24 22:27:20 -0700

    add full integration specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,3 +6,14 @@ require('pry')
 require("sinatra/activerecord")
 require('./lib/list')
 require('./lib/task')
+
+RSpec.configure do |config|
+  config.after(:each) do
+    Task.all().each() do |task|
+      task.destroy()
+    end
+    List.all().each() do |list|
+      list.destroy()
+    end
+  end
+end
--- spec/todo_integration_spec.rb
+++ spec/todo_integration_spec.rb
@@ -7,8 +7,72 @@ require('spec_helper')
 describe('adding a new list', {:type => :feature}) do
   it('allows a user to add a list ') do
     visit('/')
-    fill_in('name', :with => 'Jane Brown')
+    fill_in('name', :with => 'Homework')
     click_button('Add list')
-    expect(page).to have_content('Jane Brown')
+    expect(page).to have_content('Homework')
+  end
+end
+
+describe('updating a list', {:type => :feature}) do
+  it('allows a user to update a list ') do
+    list = List.new({:name => 'Epicodus Work'})
+    list.save()
+    visit('/')
+    click_link('Epicodus Work')
+    fill_in('name', :with => 'Homework')
+    click_button('Update list')
+    expect(page).to have_content('Homework')
+  end
+end
+
+describe('deleting a list', {:type => :feature}) do
+  it('allows a user to delete a list ') do
+    list = List.new({:name => 'Schoolwork'})
+    list.save()
+    visit('/')
+    click_link('Schoolwork')
+    click_button('Delete list')
+    expect(page).to have_no_content('School Work')
+  end
+end
+
+describe('adding a task to a list', {:type => :feature}) do
+  it('allows a user to add a task to a list ') do
+    list = List.new({:name => 'Work'})
+    list.save()
+    visit('/')
+    click_link('Work')
+    fill_in('description', :with => 'Stuff')
+    click_button('Add task')
+    expect(page).to have_content('Stuff')
+  end
+end
+
+describe('updating a task', {:type => :feature}) do
+  it('allows a user to update a list ') do
+    list = List.new({:name => 'Coding HW'})
+    list.save()
+    task = Task.new({:description => 'watch video', :list_id => list.id()})
+    task.save()
+    visit('/')
+    click_link('Coding HW')
+    click_link('watch video')
+    fill_in('description', :with => 'Watch video and read texts')
+    click_button('Update task')
+    expect(page).to have_content('Watch video and read texts')
+  end
+end
+
+describe('deleting a task', {:type => :feature}) do
+  it('allows a user to delete a task ') do
+    list = List.new({:name => 'More stuff'})
+    list.save()
+    list.tasks.new({:description => 'do the stuff'})
+    list.save()
+    visit('/')
+    click_link('More stuff')
+    click_link('do the stuff')
+    click_button('Delete task')
+    expect(page).to have_no_content('do the stuff')
   end
 end
